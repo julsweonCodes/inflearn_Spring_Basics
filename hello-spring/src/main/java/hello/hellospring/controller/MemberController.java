16633a48d7679b2b3cdb26b606addedd44b8e815
julsweonCodes <julsweon@gmail.com>
2023-01-01 23:09:46 +0900

    DI 설명

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -21,4 +21,32 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    /*
+    <DI>
+     #1. 필드 주입
+     스프링 뜰 때만 넣어줌, 중간에 못 바꿈, 권장x
+     @Autowired private MemberService memberService;
+
+
+     #2. Setter 주입
+     누군가가 memberController를 호출했을 때 메서드가 public으로 열려 있어야만 한다
+     -> 로딩 시점 이후 중간에 변경됐을 경우 문제 발생 가능
+     private MemberService memberService;
+
+     @Autowired
+     public void setMemberService(MemberService memberService) {
+         this.memberService = memberService;
+    }
+
+
+    #3. 생성자 이용 주입 (조립)
+     *위에서 사용한 방법 - 가장 권장하는 방법.
+     의존 관계가 실행 중 동적으로 변하는 경우는 거의 없음 (i.e. memberService -> memberRepository)
+
+     ** 프로젝트에서 아직 DB가 선정되지 않아 MemberRepository 인터페이스를 사용하는 것으로 가정
+        후에 나머지 코드를 건들지 않고 SpringConfig 파일의 구현 클래스 코드 수정만으로 쉽게 변경할 수 있게 하기 위함.
+        -> SpringConfig.java에서 MemberRepository에서 return DBMemberReposity()를 반환하는 것으로..
+     */
+
 }
